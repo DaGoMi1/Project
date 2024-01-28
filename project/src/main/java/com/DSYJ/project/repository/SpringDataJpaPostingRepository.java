16262dda00035d8e1807f10b01aea17288fbd369
DaGeom Lee <dlekrja4523@gmail.com>
2024-01-28 16:34:41 +0900

    feat: first commit
    - springboot 프로젝트 빌드

--- project/src/main/java/com/DSYJ/project/repository/SpringDataJpaPostingRepository.java
+++ project/src/main/java/com/DSYJ/project/repository/SpringDataJpaPostingRepository.java
@@ -3,5 +3,8 @@ package com.DSYJ.project.repository;
 import com.DSYJ.project.domain.Posting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SpringDataJpaPostingRepository extends JpaRepository<Posting, Long>{
+    List<Posting> findByBoardType(String boardType);
 }
