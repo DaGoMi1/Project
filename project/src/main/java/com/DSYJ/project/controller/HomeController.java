ae89263e1f3962eea660e841bb567ca013104357
DaGeom Lee <dlekrja4523@gmail.com>
2024-02-05 13:00:34 +0900

    feat: first commit
    - springboot 프로젝트 빌드

--- project/src/main/java/com/DSYJ/project/controller/HomeController.java
+++ project/src/main/java/com/DSYJ/project/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.DSYJ.project.controller;
 
 import com.DSYJ.project.domain.Member;
+import com.DSYJ.project.domain.Posting;
 import com.DSYJ.project.dto.CustomUserDetails;
+import com.DSYJ.project.service.PostingService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,12 +16,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final PostingService postingService;
+
+    public HomeController(PostingService postingService) {
+        this.postingService = postingService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Posting> freeBoardList = postingService.findByBoardType("free"); // 자유게시판 목록을 가져온다
+        model.addAttribute("freeBoardList", freeBoardList);
         return "home";
     }
 }
